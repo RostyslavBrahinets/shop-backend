03730a13b85f27e44713c064f0d87a9d36e4dbda
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-04-25 13:28:25 +0300

    Add exceptions

---
+++ domain/src/main/java/com/shop/exceptions/ValidationException.java
@@ -0,0 +1,14 @@
+package com.shop.exceptions;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException() {
+    }
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
