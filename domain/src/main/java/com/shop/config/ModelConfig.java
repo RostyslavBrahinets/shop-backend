3064ba65d4912b7fe28a1c9b757e2da151cb2d5e
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-04-25 11:59:00 +0300

    Add configuration for models

---
+++ domain/src/main/java/com/shop/config/ModelConfig.java
@@ -0,0 +1,33 @@
+package com.shop.config;
+
+import com.shop.models.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+public class ModelConfig {
+    @Bean
+    public Basket basket() {
+        return new Basket();
+    }
+
+    @Bean
+    public Contact contact() {
+        return new Contact();
+    }
+
+    @Bean
+    public Person person() {
+        return new Person();
+    }
+
+    @Bean
+    public Product product() {
+        return new Product();
+    }
+
+    @Bean
+    public Wallet wallet() {
+        return new Wallet();
+    }
+}
