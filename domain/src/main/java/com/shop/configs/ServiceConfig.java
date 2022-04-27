7bb3dc394088c99f6be232185ef8cf826e05aea1
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-04-27 18:23:46 +0300

    Add service for products

--- domain/src/main/java/com/shop/configs/ServiceConfig.java
+++ domain/src/main/java/com/shop/configs/ServiceConfig.java
@@ -3,12 +3,15 @@ package com.shop.configs;
 import com.shop.repositories.BasketRepository;
 import com.shop.repositories.ContactRepository;
 import com.shop.repositories.PersonRepository;
+import com.shop.repositories.ProductRepository;
 import com.shop.services.BasketService;
 import com.shop.services.ContactService;
 import com.shop.services.PersonService;
+import com.shop.services.ProductService;
 import com.shop.validators.BasketValidator;
 import com.shop.validators.ContactValidator;
 import com.shop.validators.PersonValidator;
+import com.shop.validators.ProductValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -38,5 +41,13 @@ public class ServiceConfig {
         PersonValidator personValidator
     ) {
         return new PersonService(personRepository, personValidator);
+    }
+
+    @Bean
+    public ProductService personService(
+        ProductRepository productRepository,
+        ProductValidator productValidator
+    ) {
+        return new ProductService(productRepository, productValidator);
     }
 }
---
+++ domain/src/main/java/com/shop/services/ProductService.java
@@ -0,0 +1,54 @@
+package com.shop.services;
+
+import com.shop.exceptions.NotFoundException;
+import com.shop.models.Product;
+import com.shop.repositories.ProductRepository;
+import com.shop.validators.ProductValidator;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProductService {
+    private final ProductRepository productRepository;
+    private final ProductValidator validator;
+
+    public ProductService(
+        ProductRepository productRepository,
+        ProductValidator validator
+    ) {
+        this.productRepository = productRepository;
+        this.validator = validator;
+    }
+
+    public List<Product> getProducts() {
+        return productRepository.getProducts();
+    }
+
+    public Product addProduct(Product product) {
+        validator.validate(product);
+        productRepository.addProduct(product);
+        return product;
+    }
+
+    public Product updateProduct(int id, Product product) {
+        validator.validate(id);
+        validator.validate(product);
+        productRepository.updateProduct(id, product);
+        return product;
+    }
+
+    public void deleteProduct(int id) {
+        validator.validate(id);
+        productRepository.deleteProduct(id);
+    }
+
+    public Product getProduct(int id) {
+        validator.validate(id);
+        Optional<Product> product = productRepository.getProduct(id);
+        if (product.isEmpty()) {
+            throw new NotFoundException("Product not found");
+        } else {
+            return product.get();
+        }
+    }
+}
