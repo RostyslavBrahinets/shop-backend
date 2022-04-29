1d8efb07ef7c11d15ef1655c191c9ec5aeaa714d
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-04-29 17:42:56 +0300

    Add rest controller for contacts

---
+++ web-app/src/main/java/com/shop/controllers/ContactController.java
@@ -0,0 +1,45 @@
+package com.shop.controllers;
+
+import com.shop.configs.AppConfig;
+import com.shop.models.Contact;
+import com.shop.services.ContactService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = ContactController.CONTACTS_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class ContactController {
+    public static final String CONTACTS_URL = "/contacts";
+    public static final AnnotationConfigApplicationContext applicationContext =
+        new AnnotationConfigApplicationContext(AppConfig.class);
+    public static final ContactService contactService =
+        applicationContext.getBean(ContactService.class);
+
+    @GetMapping
+    public List<Contact> findAllContact() {
+        return contactService.getContacts();
+    }
+
+    @GetMapping("/{id}")
+    public Contact findByIdContact(@PathVariable int id) {
+        return contactService.getContact(id);
+    }
+
+    @PostMapping
+    public Contact saveContact(
+        @RequestBody Contact contact,
+        @RequestBody int personId
+    ) {
+        return contactService.addContact(contact, personId);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteContact(@PathVariable int id) {
+        contactService.deleteContact(id);
+    }
+}
