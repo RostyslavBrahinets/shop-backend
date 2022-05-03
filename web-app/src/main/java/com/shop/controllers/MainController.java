7d47a783a730f9a3bc1f72c9cb5a5256867e0dec
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-03 15:13:22 +0300

    Add rest controller for main

---
+++ web-app/src/main/java/com/shop/controllers/MainController.java
@@ -0,0 +1,29 @@
+package com.shop.controllers;
+
+import com.shop.configs.AppConfig;
+import com.shop.models.Product;
+import com.shop.services.ProductService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class MainController {
+    public static final AnnotationConfigApplicationContext applicationContext =
+        new AnnotationConfigApplicationContext(AppConfig.class);
+    public static final ProductService productService =
+        applicationContext.getBean(ProductService.class);
+
+    @GetMapping
+    public List<Product> index() {
+        List<Product> randomProducts = productService.getRandomProducts(10);
+        System.out.println(randomProducts);
+        return randomProducts;
+    }
+}
+
