32780673bc2cd099326dd604cda12062afc4ac67
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-05 21:56:30 +0300

    Create model for roles

--- domain/src/main/java/com/shop/models/Role.java
+++ domain/src/main/java/com/shop/models/Role.java
@@ -1,6 +1,64 @@
 package com.shop.models;
 
-public enum Role {
-    ADMIN,
-    USER
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Role implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 6L;
+    private long id;
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Role role = (Role) o;
+        return id == role.id
+            && Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{"
+            + "id=" + id
+            + ", name='" + name + '\''
+            + '}';
+    }
 }
