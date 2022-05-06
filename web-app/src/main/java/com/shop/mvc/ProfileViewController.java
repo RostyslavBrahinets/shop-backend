c91748a01d6fa8ba3031a2b1718f0f371061ae8f
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-06 22:13:49 +0300

    Add profile

---
+++ web-app/src/main/java/com/shop/mvc/ProfileViewController.java
@@ -0,0 +1,31 @@
+package com.shop.mvc;
+
+import com.shop.models.Person;
+import com.shop.services.PersonService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/profile")
+public class ProfileViewController {
+    private final PersonService personService;
+
+    public ProfileViewController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @GetMapping()
+    public String index(
+        @AuthenticationPrincipal UserDetails userDetails,
+        Model model
+    ) {
+        Person person = personService.getPerson(userDetails.getUsername());
+        model.addAttribute("id", person.getId());
+        return "profile/index";
+    }
+}
