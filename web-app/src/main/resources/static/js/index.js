ebfc6d21dc555a08a2acf08728bf7344d19ddb09
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-06 19:35:19 +0300

    Refactor template for main page

---
+++ web-app/src/main/resources/static/js/index.js
@@ -0,0 +1,28 @@
+fetch('http://localhost:8080/web-api/', {
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    method: 'GET'
+})
+    .then(response => response.ok ? response.json() : alert(error))
+    .then(data => {
+        const links = data.map(({id}) => `http://localhost:8080/web-api/products/${id}`);
+
+        let formattedData = data.map(({name}) => `${name}`);
+        formattedData = formattedData.join('-').split('-');
+
+        let i = 0;
+        formattedData.forEach(element => {
+            const a = document.createElement('a');
+            const link = document.createTextNode(element);
+            const node = document.createElement('div');
+
+            a.appendChild(link);
+            a.title = element.toString();
+            a.href = links[i];
+            node.appendChild(a)
+            document.querySelector('.container').append(node);
+            i++;
+        });
+    });
