11e5f44d8b9e5aa1136138806b352cc3129cb25a
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-06 19:54:01 +0300

    Add links on main page

--- web-app/src/main/java/com/shop/mvc/MainViewController.java
+++ web-app/src/main/java/com/shop/mvc/MainViewController.java
@@ -1,12 +1,28 @@
 package com.shop.mvc;
 
+import com.shop.models.Person;
+import com.shop.services.PersonService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainViewController {
+    private final PersonService personService;
+
+    public MainViewController(PersonService personService) {
+        this.personService = personService;
+    }
+
     @GetMapping
-    public String index() {
+    public String index(
+        @AuthenticationPrincipal UserDetails userDetails,
+        Model model
+    ) {
+        Person person = personService.getPerson(userDetails.getUsername());
+        model.addAttribute("id", person.getId());
         return "index";
     }
 }
