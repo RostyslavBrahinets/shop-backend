a78a35ff094eb87211d926db89f471d1a7a50238
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-06 19:35:45 +0300

    Refactor template for products

---
+++ web-app/src/main/java/com/shop/mvc/ProductViewController.java
@@ -0,0 +1,14 @@
+package com.shop.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/products")
+public class ProductViewController {
+    @GetMapping
+    public String index() {
+        return "products/index";
+    }
+}
