0e3be03fe529a55fe1083a84773db3a4300c300c
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-06 16:11:08 +0300

    Add validator for roles

---
+++ domain/src/main/java/com/shop/validators/RoleValidator.java
@@ -0,0 +1,13 @@
+package com.shop.validators;
+
+import com.shop.exceptions.NotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoleValidator {
+    public void validate(long id) {
+        if (id < 1 || id > 2) {
+            throw new NotFoundException("Role not found");
+        }
+    }
+}
