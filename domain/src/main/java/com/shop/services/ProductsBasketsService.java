ec3885420934414a837d1f776d5c59200abdf494
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-09 16:47:50 +0300

    Refactor 'ProductsBasketsService'

--- domain/src/main/java/com/shop/services/ProductsBasketsService.java
+++ domain/src/main/java/com/shop/services/ProductsBasketsService.java
@@ -3,8 +3,6 @@ package com.shop.services;
 import com.shop.exceptions.NotFoundException;
 import com.shop.models.Basket;
 import com.shop.models.Product;
-import com.shop.repositories.BasketRepository;
-import com.shop.repositories.ProductRepository;
 import com.shop.repositories.ProductsBasketsRepository;
 import com.shop.validators.BasketValidator;
 import com.shop.validators.ProductValidator;
@@ -16,69 +14,74 @@ import java.util.Optional;
 @Service
 public class ProductsBasketsService {
     private final ProductsBasketsRepository productsBasketsRepository;
-    private final BasketRepository basketRepository;
-    private final ProductRepository productRepository;
+    private final BasketService basketService;
+    private final ProductService productService;
     private final ProductValidator productValidator;
     private final BasketValidator basketValidator;
 
     public ProductsBasketsService(
         ProductsBasketsRepository productsBasketsRepository,
-        BasketRepository basketRepository,
-        ProductRepository productRepository,
+        BasketService basketService,
+        ProductService productService,
         ProductValidator productValidator,
         BasketValidator basketValidator
     ) {
         this.productsBasketsRepository = productsBasketsRepository;
-        this.basketRepository = basketRepository;
-        this.productRepository = productRepository;
+        this.basketService = basketService;
+        this.productService = productService;
         this.productValidator = productValidator;
         this.basketValidator = basketValidator;
     }
 
-    public List<Product> getProductsFromBasket(long idBasket) {
-        basketValidator.validate(idBasket);
-        return productsBasketsRepository.getProductsFromBasket(idBasket);
+    public List<Product> getProductsFromBasket(long basketId) {
+        basketValidator.validate(basketId);
+        return productsBasketsRepository.getProductsFromBasket(basketId);
     }
 
-    public long addProductToBasket(long idProduct, long idBasket) {
-        productValidator.validate(idProduct);
-        basketValidator.validate(idBasket);
-        productsBasketsRepository.addProduct(idProduct, idProduct);
+    public long addProductToBasket(long productId, long basketId) {
+        productValidator.validate(productId);
+        basketValidator.validate(basketId);
+        productsBasketsRepository.addProductToBasket(productId, basketId);
 
-        Optional<Product> productOptional = productRepository.getProduct(idProduct);
-        Optional<Basket> basketOptional = basketRepository.getBasket(idBasket);
+        Product product = productService.getProduct(productId);
+        Basket basket = basketService.getBasket(basketId);
 
-        if (basketOptional.isPresent()) {
-            Basket basket = basketOptional.get();
-            List<Product> products = basket.getProducts();
-            productOptional.ifPresent(products::add);
-            basket.setProducts(products);
-        }
+        List<Product> products = basket.getProducts();
+        products.add(product);
+        basket.setProducts(products);
 
-        return idProduct;
+        double newTotalCost = basket.getTotalCost();
+        newTotalCost += product.getPrice();
+        basket.setTotalCost(newTotalCost);
+        basketService.updateBasket(basketId, basket);
+
+        return productId;
     }
 
-    public void deleteProductFromBasket(long idProduct, long idBasket) {
-        productValidator.validate(idProduct);
-        basketValidator.validate(idBasket);
-        productsBasketsRepository.deleteProduct(idProduct, idProduct);
+    public void deleteProductFromBasket(long productId, long basketId) {
+        productValidator.validate(productId);
+        basketValidator.validate(basketId);
+        productsBasketsRepository.deleteProductFromBasket(productId, productId);
 
-        Optional<Basket> basketOptional = basketRepository.getBasket(idBasket);
+        Product product = productService.getProduct(productId);
+        Basket basket = basketService.getBasket(basketId);
 
-        if (basketOptional.isPresent()) {
-            Basket basket = basketOptional.get();
-            List<Product> products = basket.getProducts();
-            products.remove((int) idProduct);
-            basket.setProducts(products);
-        }
+        List<Product> products = basket.getProducts();
+        products.remove((int) productId);
+        basket.setProducts(products);
 
+        double newTotalCost = basket.getTotalCost();
+        newTotalCost -= product.getPrice();
+        basket.setTotalCost(newTotalCost);
+        basketService.updateBasket(basketId, basket);
     }
 
-    public Product getProductFromBasket(long idProduct, long idBasket) {
-        productValidator.validate(idProduct);
-        basketValidator.validate(idBasket);
+    public Product getProductFromBasket(long productId, long basketId) {
+        productValidator.validate(productId);
+        basketValidator.validate(basketId);
 
-        Optional<Product> product = productsBasketsRepository.getProduct(idProduct, idBasket);
+        Optional<Product> product = productsBasketsRepository
+            .getProductFromBasket(productId, basketId);
         if (product.isEmpty()) {
             throw new NotFoundException("Product not found");
         } else {
