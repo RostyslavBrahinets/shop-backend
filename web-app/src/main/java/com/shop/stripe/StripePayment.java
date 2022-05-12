4117c041ca0a02d6e669c43d71dae421317dec8f
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-12 17:55:38 +0300

    Add Stripe payment

---
+++ web-app/src/main/java/com/shop/stripe/StripePayment.java
@@ -0,0 +1,36 @@
+package com.shop.stripe;
+
+import com.shop.models.Contact;
+import com.shop.models.Person;
+import com.shop.services.ContactService;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class StripePayment {
+    private final ContactService contactService;
+    @Value("${stripe.apikey}")
+    private String stripeKey;
+
+    public StripePayment(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    public Customer saveCustomer(Person person) throws StripeException {
+        Stripe.apiKey = stripeKey;
+
+        Contact contact = contactService.getContactByPerson(person.getId());
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", person.getFirstName() + " " + person.getLastName());
+        params.put("email", contact.getEmail());
+
+        return Customer.create(params);
+    }
+}
