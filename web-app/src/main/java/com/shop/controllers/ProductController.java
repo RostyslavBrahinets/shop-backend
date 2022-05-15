a68390de9cf399338562240f7dcdba05a357b53a
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-15 22:57:18 +0300

    Add deleting products

--- web-app/src/main/java/com/shop/controllers/ProductController.java
+++ web-app/src/main/java/com/shop/controllers/ProductController.java
@@ -2,10 +2,11 @@ package com.shop.controllers;
 
 import com.shop.models.Product;
 import com.shop.services.ProductService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -30,14 +31,19 @@ public class ProductController {
 
     @PostMapping
     public Product savePerson(
-        @RequestBody Product product
-    ) {
+        @RequestBody Product product,
+        HttpServletResponse response
+    ) throws IOException {
+        response.sendRedirect("/products");
         return productService.addProduct(product);
     }
 
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteProduct(@PathVariable int id) {
+    @PostMapping("/{id}")
+    public void deleteProduct(
+        @PathVariable int id,
+        HttpServletResponse response
+    ) throws IOException {
         productService.deleteProduct(id);
+        response.sendRedirect("/products");
     }
 }
