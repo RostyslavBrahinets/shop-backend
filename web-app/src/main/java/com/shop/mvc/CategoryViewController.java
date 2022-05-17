c269b649f4fa3f2afe64967aa8bd01cf02a67696
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-17 17:45:32 +0300

    Add view controller for categories

---
+++ web-app/src/main/java/com/shop/mvc/CategoryViewController.java
@@ -0,0 +1,47 @@
+package com.shop.mvc;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/categories")
+public class CategoryViewController {
+    @GetMapping
+    public String index(
+        @AuthenticationPrincipal UserDetails userDetails,
+        Model model
+    ) {
+        if (userDetails != null) {
+            GrantedAuthority role = userDetails.getAuthorities().stream().toList().get(0);
+            model.addAttribute("role", role.getAuthority());
+        } else {
+            model.addAttribute("role", "ROLE_GUEST");
+        }
+
+        return "categories/index";
+    }
+
+    @GetMapping("/{id}")
+    public String get(
+        @PathVariable long id,
+        @AuthenticationPrincipal UserDetails userDetails,
+        Model model
+    ) {
+        if (userDetails != null) {
+            GrantedAuthority role = userDetails.getAuthorities().stream().toList().get(0);
+            model.addAttribute("role", role.getAuthority());
+        } else {
+            model.addAttribute("role", "ROLE_GUEST");
+        }
+
+        model.addAttribute("id", id);
+
+        return "categories/get";
+    }
+}
