1bf1793a81a67f5b6598852547bb6430a9065c33
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-17 17:44:07 +0300

    Add validator for categories

---
+++ domain/src/main/java/com/shop/validators/CategoryValidator.java
@@ -0,0 +1,51 @@
+package com.shop.validators;
+
+import com.shop.exceptions.NotFoundException;
+import com.shop.exceptions.ValidationException;
+import com.shop.models.Category;
+import com.shop.repositories.CategoryRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CategoryValidator {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryValidator(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public void validate(Category category) {
+        String name = category.getName();
+
+        if (name == null || name.isBlank()) {
+            throw new ValidationException("Name of category is invalid");
+        }
+    }
+
+    public void validate(long id) {
+        List<Long> ids = new ArrayList<>();
+
+        for (Category category : categoryRepository.getCategories()) {
+            ids.add(category.getId());
+        }
+
+        if (id < 1 || !ids.contains(id)) {
+            throw new NotFoundException("Category not found");
+        }
+    }
+
+    public void validate(String name) {
+        List<String> names = new ArrayList<>();
+
+        for (Category category : categoryRepository.getCategories()) {
+            names.add(category.getName());
+        }
+
+        if (!names.contains(name)) {
+            throw new NotFoundException("Category not found");
+        }
+    }
+}
