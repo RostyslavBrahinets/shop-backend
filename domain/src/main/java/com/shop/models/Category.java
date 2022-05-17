c86b7fbee9f6743b453c903a6f20d3b98d3fb855
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-17 17:43:25 +0300

    Add model for categories

--- domain/src/main/java/com/shop/models/Category.java
+++ domain/src/main/java/com/shop/models/Category.java
@@ -1,8 +1,67 @@
 package com.shop.models;
 
-public enum Category {
-    BOOK,
-    CLOTHES,
-    ELECTRONICS,
-    SPORT
+import org.springframework.stereotype.Component;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Component
+public class Category implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 7L;
+    private long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Category role = (Category) o;
+        return id == role.id
+            && Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{"
+            + "id=" + id
+            + ", name='" + name + '\''
+            + '}';
+    }
 }
