b3077cad2c4614202c421ca6b6aa9059bfec2875
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-17 17:48:13 +0300

    Add sql scripts for product_category

---
+++ domain/src/main/resources/db/migration/product_category/V20220421162206__Create_table_product_category.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS product_category
+(
+    product_id  INTEGER NOT NULL,
+    category_id INTEGER NOT NULL
+);
---
+++ domain/src/main/resources/db/migration/product_category/V20220427144406__Insert_data_to_table_product_category.sql
@@ -0,0 +1,3 @@
+INSERT INTO product_category (product_id, category_id)
+VALUES (1, 3),
+       (2, 3);
