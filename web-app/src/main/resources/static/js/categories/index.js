31c2eccfd9492011e7ae6739e04b710c86b3f028
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-17 17:50:47 +0300

    Add template for categories

---
+++ web-app/src/main/resources/static/js/categories/index.js
@@ -0,0 +1,32 @@
+function displayCategories() {
+    fetch('http://localhost:8080/web-api/categories/', {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        method: 'GET'
+    })
+        .then(response => response.ok ? response.json() : alert(error))
+        .then(data => {
+            const links = data.map(({id}) => `http://localhost:8080/categories/${id}`);
+
+            let formattedData = data.map(({name}) => `${name}`);
+            formattedData = formattedData.join('-').split('-');
+
+            let i = 0;
+            formattedData.forEach(element => {
+                const a = document.createElement('a');
+                const link = document.createTextNode(element);
+                const node = document.createElement('div');
+
+                a.appendChild(link);
+                a.title = element.toString();
+                a.href = links[i];
+                node.appendChild(a)
+                document.querySelector('.categories-container').append(node);
+                i++;
+            });
+        });
+}
+
+displayCategories();
