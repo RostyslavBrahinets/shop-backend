12125bd9378963624ad4c7e4759368d11bdcea06
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-17 14:08:58 +0300

    Add getting user in admin panel

--- web-app/src/main/java/com/shop/mvc/AdminViewController.java
+++ web-app/src/main/java/com/shop/mvc/AdminViewController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -27,6 +28,15 @@ public class AdminViewController {
         Person person = personService.getPerson(userDetails.getUsername());
         model.addAttribute("id", person.getId());
         return "admin/index";
+    }
+
+    @GetMapping("/users/{id}")
+    public String getUser(
+        @PathVariable String id,
+        Model model
+    ) {
+        model.addAttribute("id", id);
+        return "admin/get";
     }
 
     @GetMapping("/products/add")
