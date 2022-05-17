6d20199a5bf9ffb248e6a531b44e5484ed64b8f7
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-17 17:48:27 +0300

    Update sql scripts for products

--- domain/src/main/resources/db/migration/product/V20220427144405__Insert_data_to_table_product.sql
+++ domain/src/main/resources/db/migration/product/V20220427144405__Insert_data_to_table_product.sql
@@ -1,3 +1,3 @@
-INSERT INTO product (name, describe, price, category, in_stock)
-VALUES ('Samsung A02', 'Smartphone', 4300.0, 'ELECTRONICS', true),
-       ('iPhone 13 Pro Max', 'Smartphone', 30000.0, 'ELECTRONICS', true);
+INSERT INTO product (name, describe, price, in_stock)
+VALUES ('Samsung A02', 'Smartphone', 4300.0, true),
+       ('iPhone 13 Pro Max', 'Smartphone', 30000.0, true);
