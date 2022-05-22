91bf25171a6f4db4490fdcf1ddd424e812d4c6cf
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-23 00:33:57 +0300

    Add barcode to sql scripts for products

--- domain/src/main/resources/db/migration/product/V20220421162205__Create_table_product.sql
+++ domain/src/main/resources/db/migration/product/V20220421162205__Create_table_product.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS product
     name     VARCHAR(256) NOT NULL,
     describe VARCHAR(256) NOT NULL,
     price    REAL         NOT NULL,
+    barcode  VARCHAR(256) NOT NULL UNIQUE,
     in_stock BOOL         NOT NULL,
     image    OID
 );
--- domain/src/main/resources/db/migration/product/V20220427144405__Insert_data_to_table_product.sql
+++ domain/src/main/resources/db/migration/product/V20220427144405__Insert_data_to_table_product.sql
@@ -1,3 +1,3 @@
-INSERT INTO product (name, describe, price, in_stock)
-VALUES ('Samsung A02', 'Smartphone', 4300.0, true),
-       ('iPhone 13 Pro Max', 'Smartphone', 30000.0, true);
+INSERT INTO product (name, describe, price, barcode, in_stock)
+VALUES ('Samsung A02', 'Smartphone', 4300.0, '125002741350', true),
+       ('iPhone 13 Pro Max', 'Smartphone', 30000.0, '105002741351', true);
