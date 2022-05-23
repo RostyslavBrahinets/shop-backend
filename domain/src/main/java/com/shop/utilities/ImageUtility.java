ea5788e29e33491b0322302e3110959da6ebcd1a
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-24 00:23:59 +0300

    Add method for importing image to bytes

---
+++ domain/src/main/java/com/shop/utilities/ImageUtility.java
@@ -0,0 +1,20 @@
+package com.shop.utilities;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ImageUtility {
+    public static byte[] imageToBytes(File file) throws IOException {
+        FileInputStream fis = new FileInputStream(file);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        byte[] buf = new byte[1024];
+
+        for (int readNum; (readNum = fis.read(buf)) != -1; ) {
+            bos.write(buf, 0, readNum);
+        }
+
+        return bos.toByteArray();
+    }
+}
