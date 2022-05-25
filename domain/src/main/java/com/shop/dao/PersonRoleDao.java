917499210a3ccb828cba7a45cdc0900b79e4b156
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-25 12:12:02 +0300

    Refactor 'PersonRoleDao'

--- domain/src/main/java/com/shop/dao/PersonRoleDao.java
+++ domain/src/main/java/com/shop/dao/PersonRoleDao.java
@@ -1,6 +1,5 @@
 package com.shop.dao;
 
-import com.shop.db.DatabaseTemplate;
 import com.shop.models.Role;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -11,24 +10,18 @@ import java.util.Optional;
 
 @Component
 public class PersonRoleDao {
-    private final NamedParameterJdbcTemplate jdbcTemplate = DatabaseTemplate.getJdbcTemplate();
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
-    public Optional<Role> getRole(long personId) {
-        return jdbcTemplate.query(
-                "SELECT * FROM role r, person_role pr "
-                    + "WHERE pr.role_id=r.id and pr.person_id=:person_id",
-                Map.of("person_id", personId),
-                new BeanPropertyRowMapper<>(Role.class)
-            )
-            .stream().findAny();
+    public PersonRoleDao(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void addRoleForPerson(long personId, long roleId) {
+    public void saveRoleForPerson(long personId, long roleId) {
         jdbcTemplate.update(
             "INSERT INTO person_role (person_id, role_id) VALUES (:person_id, :role_id)",
-            Map.of(
-                "person_id", personId,
-                "role_id", roleId
+            Map.ofEntries(
+                Map.entry("person_id", personId),
+                Map.entry("role_id", roleId)
             )
         );
     }
@@ -36,10 +29,20 @@ public class PersonRoleDao {
     public void updateRoleForPerson(long personId, long roleId) {
         jdbcTemplate.update(
             "UPDATE person_role SET role_id=:role_id WHERE person_id=:person_id",
-            Map.of(
-                "person_id", personId,
-                "role_id", roleId
+            Map.ofEntries(
+                Map.entry("person_id", personId),
+                Map.entry("role_id", roleId)
             )
         );
+    }
+
+    public Optional<Role> findRoleByPerson(long personId) {
+        return jdbcTemplate.query(
+                "SELECT * FROM role r, person_role pr "
+                    + "WHERE pr.role_id=r.id and pr.person_id=:person_id",
+                Map.ofEntries(Map.entry("person_id", personId)),
+                new BeanPropertyRowMapper<>(Role.class)
+            )
+            .stream().findAny();
     }
 }
