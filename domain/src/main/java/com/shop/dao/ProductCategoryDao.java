3b15ff12c8ea21d116adcb7b2c2f37392e8fde4d
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-25 12:12:22 +0300

    Refactor 'ProductCategoryDao'

--- domain/src/main/java/com/shop/dao/ProductCategoryDao.java
+++ domain/src/main/java/com/shop/dao/ProductCategoryDao.java
@@ -1,6 +1,5 @@
 package com.shop.dao;
 
-import com.shop.db.DatabaseTemplate;
 import com.shop.models.Category;
 import com.shop.models.Product;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -13,35 +12,39 @@ import java.util.Optional;
 
 @Component
 public class ProductCategoryDao {
-    private final NamedParameterJdbcTemplate jdbcTemplate = DatabaseTemplate.getJdbcTemplate();
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
-    public Optional<Category> getCategoryForProduct(long productId) {
-        return jdbcTemplate.query(
-                "SELECT * FROM category c, product_category pc "
-                    + "WHERE pc.category_id = c.id AND pc.product_id=:product_id",
-                Map.of("product_id", productId),
-                new BeanPropertyRowMapper<>(Category.class)
-            )
-            .stream().findAny();
+    public ProductCategoryDao(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void addProductToCategory(long productId, long categoryId) {
-        jdbcTemplate.update(
-            "INSERT INTO product_category (product_id, category_id)"
-                + " VALUES (:product_id, :category_id)",
-            Map.of(
-                "product_id", productId,
-                "category_id", categoryId
-            )
-        );
-    }
-
-    public List<Product> getProductsInCategory(long categoryId) {
+    public List<Product> findAllProductsInCategory(long categoryId) {
         return jdbcTemplate.query(
             "SELECT p.* FROM product p, product_category pc "
                 + "WHERE pc.product_id=p.id AND pc.category_id=:category_id",
-            Map.of("category_id", categoryId),
+            Map.ofEntries(Map.entry("category_id", categoryId)),
             new BeanPropertyRowMapper<>(Product.class)
         );
+    }
+
+    public void saveProductToCategory(long productId, long categoryId) {
+        jdbcTemplate.update(
+            "INSERT INTO product_category (product_id, category_id)"
+                + " VALUES (:product_id, :category_id)",
+            Map.ofEntries(
+                Map.entry("product_id", productId),
+                Map.entry("category_id", categoryId)
+            )
+        );
+    }
+
+    public Optional<Category> findCategoryForProduct(long productId) {
+        return jdbcTemplate.query(
+                "SELECT * FROM category c, product_category pc "
+                    + "WHERE pc.category_id = c.id AND pc.product_id=:product_id",
+                Map.ofEntries(Map.entry("product_id", productId)),
+                new BeanPropertyRowMapper<>(Category.class)
+            )
+            .stream().findAny();
     }
 }
