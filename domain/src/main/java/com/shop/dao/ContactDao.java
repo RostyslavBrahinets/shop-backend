7b4c7fdda5118d2f9de37829c83f7cdf6c89ca48
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-25 12:11:22 +0300

    Refactor 'ContactDao'

--- domain/src/main/java/com/shop/dao/ContactDao.java
+++ domain/src/main/java/com/shop/dao/ContactDao.java
@@ -1,6 +1,5 @@
 package com.shop.dao;
 
-import com.shop.db.DatabaseTemplate;
 import com.shop.models.Contact;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -12,60 +11,69 @@ import java.util.Optional;
 
 @Component
 public class ContactDao {
-    private final NamedParameterJdbcTemplate jdbcTemplate = DatabaseTemplate.getJdbcTemplate();
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
-    public List<Contact> getContacts() {
+    public ContactDao(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Contact> findAll() {
         return jdbcTemplate.query(
             "SELECT * FROM contact",
             new BeanPropertyRowMapper<>(Contact.class)
         );
     }
 
-    public void addContact(Contact contact, long personId) {
+    public void save(
+        String email,
+        String phone,
+        String password,
+        long personId
+    ) {
         jdbcTemplate.update(
             "INSERT INTO contact (email, phone, password, person_id) "
                 + "VALUES (:email, :phone, :password, :person_id)",
-            Map.of(
-                "email", contact.getEmail(),
-                "phone", contact.getPhone(),
-                "password", contact.getPassword(),
-                "person_id", personId
+            Map.ofEntries(
+                Map.entry("email", email),
+                Map.entry("phone", phone),
+                Map.entry("password", password),
+                Map.entry("person_id", personId)
             )
         );
     }
 
-    public void deleteContact(long id) {
-        jdbcTemplate.update(
-            "DELETE FROM contact WHERE id=:id",
-            Map.of("id", id)
-        );
-    }
-
-    public Optional<Contact> getContact(long id) {
-        return jdbcTemplate.query(
-                "SELECT * FROM contact WHERE id=:id",
-                Map.of("id", id),
-                new BeanPropertyRowMapper<>(Contact.class)
-            )
-            .stream().findAny();
-    }
-
-    public Optional<Contact> getContactByPerson(long personId) {
-        return jdbcTemplate.query(
-                "SELECT * FROM contact WHERE person_id=:person_id",
-                Map.of("person_id", personId),
-                new BeanPropertyRowMapper<>(Contact.class)
-            )
-            .stream().findAny();
-    }
-
-    public void updateContact(long id, Contact updatedContact) {
+    public void update(long id, String phone) {
         jdbcTemplate.update(
             "UPDATE contact SET phone=:phone WHERE person_id=:id",
-            Map.of(
-                "phone", updatedContact.getPhone(),
-                "id", id
+            Map.ofEntries(
+                Map.entry("phone", phone),
+                Map.entry("id", id)
             )
         );
+    }
+
+    public void delete(long id) {
+        jdbcTemplate.update(
+            "DELETE FROM contact WHERE id=:id",
+            Map.ofEntries(Map.entry("id", id))
+        );
+    }
+
+    public Optional<Contact> findById(long id) {
+        return jdbcTemplate.query(
+                "SELECT * FROM contact WHERE id=:id",
+                Map.ofEntries(Map.entry("id", id)),
+                new BeanPropertyRowMapper<>(Contact.class)
+            )
+            .stream().findAny();
+    }
+
+    public Optional<Contact> findByPerson(long personId) {
+        return jdbcTemplate.query(
+                "SELECT * FROM contact WHERE person_id=:person_id",
+                Map.ofEntries(Map.entry("person_id", personId)),
+                new BeanPropertyRowMapper<>(Contact.class)
+            )
+            .stream().findAny();
     }
 }
