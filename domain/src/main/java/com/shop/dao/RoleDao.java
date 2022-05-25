5803f818d0abcb0e48e1aaa5bfe418aa818eaf41
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-25 12:13:26 +0300

    Refactor 'RoleDao'

--- domain/src/main/java/com/shop/dao/RoleDao.java
+++ domain/src/main/java/com/shop/dao/RoleDao.java
@@ -1,6 +1,5 @@
 package com.shop.dao;
 
-import com.shop.db.DatabaseTemplate;
 import com.shop.models.Role;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -11,12 +10,16 @@ import java.util.Optional;
 
 @Component
 public class RoleDao {
-    private final NamedParameterJdbcTemplate jdbcTemplate = DatabaseTemplate.getJdbcTemplate();
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
-    public Optional<Role> getRoleByName(String name) {
+    public RoleDao(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Optional<Role> findByName(String name) {
         return jdbcTemplate.query(
                 "SELECT * FROM role WHERE name=:name",
-                Map.of("name", name),
+                Map.ofEntries(Map.entry("name", name)),
                 new BeanPropertyRowMapper<>(Role.class)
             )
             .stream().findAny();
