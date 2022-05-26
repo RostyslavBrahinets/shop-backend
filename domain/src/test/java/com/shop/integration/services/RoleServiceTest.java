4fdc48f04bafa6401be2d0789b30da9c50683f47
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-27 00:22:09 +0300

    Add tests for service of roles

---
+++ domain/src/test/java/com/shop/integration/services/RoleServiceTest.java
@@ -0,0 +1,43 @@
+package com.shop.integration.services;
+
+import com.shop.models.Role;
+import com.shop.repositories.RoleRepository;
+import com.shop.services.RoleService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+class RoleServiceTest {
+    @Mock
+    private RoleRepository roleRepository;
+
+    private RoleService roleService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        roleService = new RoleService(
+            roleRepository
+        );
+    }
+
+    @Test
+    @DisplayName("Role was found by name")
+    void role_was_found_by_name() {
+        when(roleRepository.findByName("name")).thenReturn(
+            Optional.of(Role.of("name").withId(1))
+        );
+
+        Role role = roleService.findByName("name");
+
+        assertThat(role).isEqualTo(new Role(1, "name"));
+    }
+}
