0f508814d59adb816ee2510eb07754d597165124
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-26 16:46:09 +0300

    Add test for context configuration of ApplicationStarter

---
+++ web-app/src/test/java/com/shop/ApplicationStarterTest.java
@@ -0,0 +1,14 @@
+package com.shop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationStarterTest {
+    @Test
+    void contextLoads() {
+        Assertions.assertDoesNotThrow(() -> {
+        });
+    }
+}
