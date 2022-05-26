b005f1940fb629c5d82f9b37ff2e7256fe31a456
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-27 00:21:34 +0300

    Add tests for service of person_role

---
+++ domain/src/test/java/com/shop/integration/services/PersonRoleServiceTest.java
@@ -0,0 +1,42 @@
+package com.shop.integration.services;
+
+import com.shop.repositories.PersonRoleRepository;
+import com.shop.services.PersonRoleService;
+import com.shop.validators.PersonValidator;
+import com.shop.validators.RoleValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+
+class PersonRoleServiceTest {
+    @Mock
+    private PersonRoleRepository personRoleRepository;
+    @Mock
+    private PersonValidator personValidator;
+    @Mock
+    private RoleValidator roleValidator;
+
+    private PersonRoleService personRoleService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        personRoleService = new PersonRoleService(
+            personRoleRepository,
+            personValidator,
+            roleValidator
+        );
+    }
+
+    @Test
+    @DisplayName("Role for person was saved")
+    void role_for_person_was_saved() {
+        personRoleService.saveRoleForPerson(1, 1);
+        verify(personRoleRepository).saveRoleForPerson(1, 1);
+    }
+}
