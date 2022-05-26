8815c734df6dbd09d7d98818f9f5c53b7b74008f
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-26 14:02:13 +0300

    Add tests for dao of product

---
+++ domain/src/test/java/com/shop/dao/ProductDaoTest.java
@@ -0,0 +1,297 @@
+package com.shop.dao;
+
+import com.shop.configs.DatabaseConfig;
+import com.shop.models.Product;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@JdbcTest
+@ContextConfiguration(classes = {
+    DatabaseConfig.class
+})
+@Sql(scripts = {
+    "classpath:db/migration/product/V20220421162205__Create_table_product.sql"
+})
+public class ProductDaoTest {
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+    private ProductDao productDao;
+
+    @BeforeEach
+    void setUp() {
+        productDao = new ProductDao(jdbcTemplate);
+    }
+
+    @AfterEach
+    void tearDown() {
+        JdbcTestUtils.dropTables(
+            jdbcTemplate.getJdbcTemplate(),
+            "product"
+        );
+    }
+
+    private int fetchProductsCount() {
+        return JdbcTestUtils.countRowsInTable(
+            jdbcTemplate.getJdbcTemplate(),
+            "product"
+        );
+    }
+
+    @Test
+    @DisplayName("Save product")
+    void save_product() {
+        productDao.save(
+            "name",
+            "describe",
+            0,
+            "123",
+            true,
+            new byte[]{1, 1, 1}
+        );
+
+        var productsCount = fetchProductsCount();
+
+        assertThat(productsCount).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Save multiple products")
+    void save_multiple_products() {
+        productDao.save(
+            "name",
+            "describe",
+            0,
+            "123",
+            true,
+            new byte[]{1, 1, 1}
+        );
+        productDao.save(
+            "name",
+            "describe",
+            0,
+            "456",
+            true,
+            new byte[]{1, 1, 1}
+        );
+
+        var productsCount = fetchProductsCount();
+
+        assertThat(productsCount).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("Product by id was not found")
+    void product_by_id_was_not_found() {
+        Optional<Product> product = productDao.findById(1);
+
+        assertThat(product).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Product by id was found")
+    void product_by_id_was_found() {
+        new SimpleJdbcInsert(jdbcTemplate.getJdbcTemplate())
+            .withTableName("product")
+            .usingGeneratedKeyColumns("id")
+            .usingColumns("name", "describe", "price", "barcode", "in_stock", "image")
+            .execute(
+                Map.ofEntries(
+                    Map.entry("name", "name"),
+                    Map.entry("describe", "describe"),
+                    Map.entry("price", 0),
+                    Map.entry("barcode", "123"),
+                    Map.entry("in_stock", true),
+                    Map.entry("image", new byte[]{1, 1, 1})
+                )
+            );
+
+        Optional<Product> product = productDao.findById(1);
+
+        assertThat(product).get().isEqualTo(
+            Product.of(
+                    "name",
+                    "describe",
+                    0,
+                    "123",
+                    true,
+                    new byte[]{1, 1, 1}
+                )
+                .withId(1));
+    }
+
+    @Test
+    @DisplayName("Product by barcode was not found")
+    void product_by_barcode_was_not_found() {
+        Optional<Product> product = productDao.findByBarcode("123");
+
+        assertThat(product).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Product by barcode was found")
+    void product_by_barcode_was_found() {
+        new SimpleJdbcInsert(jdbcTemplate.getJdbcTemplate())
+            .withTableName("product")
+            .usingGeneratedKeyColumns("id")
+            .usingColumns("name", "describe", "price", "barcode", "in_stock", "image")
+            .execute(
+                Map.ofEntries(
+                    Map.entry("name", "name"),
+                    Map.entry("describe", "describe"),
+                    Map.entry("price", 0),
+                    Map.entry("barcode", "123"),
+                    Map.entry("in_stock", true),
+                    Map.entry("image", new byte[]{1, 1, 1})
+                )
+            );
+
+        Optional<Product> product = productDao.findByBarcode("123");
+
+        assertThat(product).get().isEqualTo(
+            Product.of(
+                    "name",
+                    "describe",
+                    0,
+                    "123",
+                    true,
+                    new byte[]{1, 1, 1}
+                )
+                .withId(1));
+    }
+
+    @Test
+    @DisplayName("Find all products")
+    void find_all_products() {
+        var batchInsertParameters = SqlParameterSourceUtils.createBatch(
+            Map.ofEntries(
+                Map.entry("name", "name"),
+                Map.entry("describe", "describe"),
+                Map.entry("price", 0),
+                Map.entry("barcode", "123"),
+                Map.entry("in_stock", true),
+                Map.entry("image", new byte[]{1, 1, 1})
+            ),
+            Map.ofEntries(
+                Map.entry("name", "name"),
+                Map.entry("describe", "describe"),
+                Map.entry("price", 0),
+                Map.entry("barcode", "456"),
+                Map.entry("in_stock", true),
+                Map.entry("image", new byte[]{1, 1, 1})
+            )
+        );
+
+        new SimpleJdbcInsert(jdbcTemplate.getJdbcTemplate())
+            .withTableName("product")
+            .usingGeneratedKeyColumns("id")
+            .usingColumns("name", "describe", "price", "barcode", "in_stock", "image")
+            .executeBatch(batchInsertParameters);
+
+        List<Product> products = productDao.findAll();
+
+        assertThat(products).isEqualTo(
+            List.of(
+                Product.of(
+                        "name",
+                        "describe",
+                        0,
+                        "123",
+                        true,
+                        new byte[]{1, 1, 1}
+                    )
+                    .withId(1),
+                Product.of(
+                        "name",
+                        "describe",
+                        0,
+                        "456",
+                        true,
+                        new byte[]{1, 1, 1}
+                    )
+                    .withId(2)
+            )
+        );
+    }
+
+    @Test
+    @DisplayName("Product not deleted in case when not exists")
+    void product_not_deleted_in_case_when_not_exists() {
+        assertThatCode(() -> productDao.delete("123")).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("Product deleted")
+    void product_deleted() {
+        new SimpleJdbcInsert(jdbcTemplate.getJdbcTemplate())
+            .withTableName("product")
+            .usingGeneratedKeyColumns("id")
+            .usingColumns("name", "describe", "price", "barcode", "in_stock", "image")
+            .execute(
+                Map.ofEntries(
+                    Map.entry("name", "name"),
+                    Map.entry("describe", "describe"),
+                    Map.entry("price", 0),
+                    Map.entry("barcode", "123"),
+                    Map.entry("in_stock", true),
+                    Map.entry("image", new byte[]{1, 1, 1})
+                )
+            );
+
+        var productsCountBeforeDeletion = fetchProductsCount();
+
+        assertThat(productsCountBeforeDeletion).isEqualTo(1);
+
+        productDao.delete("123");
+
+        var productsCount = fetchProductsCount();
+
+        assertThat(productsCount).isZero();
+    }
+
+    @Test
+    @DisplayName("Save image for product")
+    void save_image_for_product() {
+        new SimpleJdbcInsert(jdbcTemplate.getJdbcTemplate())
+            .withTableName("product")
+            .usingGeneratedKeyColumns("id")
+            .usingColumns("name", "describe", "price", "barcode", "in_stock", "image")
+            .execute(
+                Map.ofEntries(
+                    Map.entry("name", "name"),
+                    Map.entry("describe", "describe"),
+                    Map.entry("price", 0),
+                    Map.entry("barcode", "123"),
+                    Map.entry("in_stock", true),
+                    Map.entry("image", new byte[]{1, 1, 1})
+                )
+            );
+
+        productDao.saveImageForProduct(new byte[]{127, 127, 127}, 1);
+
+        var updatedBasket = jdbcTemplate.queryForObject(
+            "SELECT image FROM product WHERE id=:id",
+            Map.ofEntries(Map.entry("id", 1)),
+            byte[].class
+        );
+
+        assertThat(updatedBasket).isEqualTo(new byte[]{127, 127, 127});
+    }
+}
