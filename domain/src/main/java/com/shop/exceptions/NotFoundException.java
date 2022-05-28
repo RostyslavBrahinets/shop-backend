97d63a031f20e6ad7f10722b7e08a07b340924ac
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-28 04:58:44 +0300

    Refactor exceptions

--- domain/src/main/java/com/shop/exceptions/NotFoundException.java
+++ domain/src/main/java/com/shop/exceptions/NotFoundException.java
@@ -1,14 +1,7 @@
 package com.shop.exceptions;
 
 public class NotFoundException extends RuntimeException {
-    public NotFoundException() {
-    }
-
     public NotFoundException(String message) {
         super(message);
-    }
-
-    public NotFoundException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
--- domain/src/main/java/com/shop/exceptions/ValidationException.java
+++ domain/src/main/java/com/shop/exceptions/ValidationException.java
@@ -1,14 +1,7 @@
 package com.shop.exceptions;
 
 public class ValidationException extends RuntimeException {
-    public ValidationException() {
-    }
-
     public ValidationException(String message) {
         super(message);
-    }
-
-    public ValidationException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
