afa44ea015ce380efd3b30eae89f4a79e31349c6
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-29 23:29:59 +0300

    Fix test

--- web-app/src/test/java/com/shop/ApplicationStarterTest.java
+++ web-app/src/test/java/com/shop/ApplicationStarterTest.java
@@ -2,20 +2,31 @@ package com.shop;
 
 import com.shop.configs.DatabaseConfig;
 import com.shop.utilities.ImageDatabaseUtility;
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 
 import javax.sql.DataSource;
 
 @SpringBootTest
+@EnableAutoConfiguration
 @ContextConfiguration(classes = {
     DatabaseConfig.class,
-    DataSource.class,
     ImageDatabaseUtility.class
 })
 public class ApplicationStarterTest {
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private Flyway flyway;
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
     @Test
     void contextLoads() {
         Assertions.assertDoesNotThrow(() -> {
