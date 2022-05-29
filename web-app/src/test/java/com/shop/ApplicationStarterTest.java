b0163305f6733ed8e535748e86214a3e99f232e4
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-29 23:10:14 +0300

    Fix test

--- web-app/src/test/java/com/shop/ApplicationStarterTest.java
+++ web-app/src/test/java/com/shop/ApplicationStarterTest.java
@@ -1,10 +1,17 @@
 package com.shop;
 
+import com.shop.configs.DatabaseConfig;
+import com.shop.utilities.ImageDatabaseUtility;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = {
+    DatabaseConfig.class,
+    ImageDatabaseUtility.class
+})
 public class ApplicationStarterTest {
     @Test
     void contextLoads() {
