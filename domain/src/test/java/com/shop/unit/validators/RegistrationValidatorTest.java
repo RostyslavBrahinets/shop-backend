c252cdb06c040b03f7689b308d234661198205eb
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-30 23:36:02 +0300

    Refactor 'RegistrationValidatorTest'

---
+++ domain/src/test/java/com/shop/unit/validators/RegistrationValidatorTest.java
@@ -0,0 +1,65 @@
+package com.shop.unit.validators;
+
+import com.shop.repositories.ContactRepository;
+import com.shop.repositories.PersonRepository;
+import com.shop.validators.ContactValidator;
+import com.shop.validators.PersonValidator;
+import com.shop.validators.RegistrationValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RegistrationValidatorTest {
+    private RegistrationValidator registrationValidator;
+    private PersonValidator personValidator;
+    private ContactValidator contactValidator;
+
+    @Mock
+    private PersonRepository personRepository;
+    @Mock
+    private ContactRepository contactRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        personValidator = new PersonValidator(personRepository);
+        contactValidator = new ContactValidator(contactRepository);
+
+        registrationValidator = new RegistrationValidator(
+            personValidator,
+            contactValidator
+        );
+    }
+
+    @Test
+    @DisplayName("Registration data validated without exceptions")
+    void registration_data_validated_without_exceptions() {
+        assertDoesNotThrow(
+            () -> personValidator.validate("John", "Smith")
+        );
+
+        assertDoesNotThrow(
+            () -> contactValidator.validate(
+                "test@email.com",
+                "+380000000000",
+                "password"
+            )
+        );
+
+        assertTrue(
+            () -> registrationValidator.isValidData(
+                "John",
+                "Smith",
+                "test@email.com",
+                "+380000000000",
+                "password"
+            )
+        );
+    }
+}
