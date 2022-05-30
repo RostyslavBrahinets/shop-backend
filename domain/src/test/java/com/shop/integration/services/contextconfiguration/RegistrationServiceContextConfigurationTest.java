2488f2e5652df67980f60661927a02bee40a4fbf
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-05-31 00:18:46 +0300

    Refactor 'RegistrationServiceContextConfigurationTest'

---
+++ domain/src/test/java/com/shop/integration/services/contextconfiguration/RegistrationServiceContextConfigurationTest.java
@@ -0,0 +1,95 @@
+package com.shop.integration.services.contextconfiguration;
+
+import com.shop.services.*;
+import com.shop.stripe.StripePayment;
+import com.shop.validators.RegistrationValidator;
+import com.stripe.exception.StripeException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(
+    classes = {
+        RegistrationService.class,
+        RegistrationServiceContextConfigurationTest.TestContextConfig.class
+    }
+)
+public class RegistrationServiceContextConfigurationTest {
+    @Autowired
+    private RegistrationService registrationService;
+    @Autowired
+    private RegistrationValidator registrationValidator;
+
+    @Test
+    @DisplayName("Registration")
+    void registration() throws StripeException {
+        String firstName = "John";
+        String lastName = "Smith";
+        String email = "test@email.com";
+        String phone = "+380000000000";
+        String password = "password";
+
+        registrationService.registration(
+            firstName,
+            lastName,
+            email,
+            phone,
+            password
+        );
+
+        verify(registrationValidator).isValidData(
+            firstName,
+            lastName,
+            email,
+            phone,
+            password
+        );
+    }
+
+    @TestConfiguration
+    static class TestContextConfig {
+        @Bean
+        public RegistrationValidator registrationValidator() {
+            return mock(RegistrationValidator.class);
+        }
+
+        @Bean
+        public PersonService personService() {
+            return mock(PersonService.class);
+        }
+
+        @Bean
+        public ContactService contactService() {
+            return mock(ContactService.class);
+        }
+
+        @Bean
+        public PersonRoleService personRoleService() {
+            return mock(PersonRoleService.class);
+        }
+
+        @Bean
+        public BasketService basketService() {
+            return mock(BasketService.class);
+        }
+
+        @Bean
+        public WalletService walletService() {
+            return mock(WalletService.class);
+        }
+
+        @Bean
+        public StripePayment stripePayment() {
+            return mock(StripePayment.class);
+        }
+    }
+}
