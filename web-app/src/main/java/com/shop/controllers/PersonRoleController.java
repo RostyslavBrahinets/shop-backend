d17f0d5a8cd3a0aa8d477e2b080287ea1ecbe8e7
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-08-24 14:31:39 +0300

    Refactor 'PersonRoleController'

--- web-app/src/main/java/com/shop/controllers/PersonRoleController.java
+++ web-app/src/main/java/com/shop/controllers/PersonRoleController.java
@@ -4,7 +4,6 @@ import com.shop.dto.RoleDto;
 import com.shop.models.Role;
 import com.shop.services.PersonRoleService;
 import com.shop.services.RoleService;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
