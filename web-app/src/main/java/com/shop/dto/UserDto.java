a9891c68f97ef5952214d4de2592ff06042c5df5
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-10 23:53:22 +0200

    Add dto for user

---
+++ web-app/src/main/java/com/shop/dto/UserDto.java
@@ -0,0 +1,34 @@
+package com.shop.dto;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDto {
+    private String firstName;
+    private String lastName;
+    private String phone;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
