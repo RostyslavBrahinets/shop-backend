163f6201f87a3405c756e2cb9f6685ee1e3b56a4
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-13 22:17:26 +0200

    Move files for product_category to packages 'products_carts'

--- domain/src/main/java/com/shop/product_category/ProductsCartsRepository.java
+++ domain/src/main/java/com/shop/product_category/ProductsCartsRepository.java
@@ -1,6 +1,6 @@
-package com.shop.repositories;
+package com.shop.product_category;
 
-import com.shop.models.Product;
+import com.shop.product.Product;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
--- domain/src/test/java/com/shop/products_carts/ProductsCartsRepositoryTest.java
+++ domain/src/test/java/com/shop/products_carts/ProductsCartsRepositoryTest.java
@@ -1,7 +1,7 @@
-package com.shop.integration.repositories;
+package com.shop.products_carts;
 
 import com.shop.configs.DatabaseConfig;
-import com.shop.repositories.ProductsCartsRepository;
+import com.shop.product_category.ProductsCartsRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
