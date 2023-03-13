d6bee165955dbaaa70170606243a71c81da98334
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-13 14:54:11 +0200

    Change 'person' to 'user' in 'UserDetailsServiceImpl'

--- web-app/src/main/java/com/shop/security/UserDetailsServiceImpl.java
+++ web-app/src/main/java/com/shop/security/UserDetailsServiceImpl.java
@@ -1,11 +1,8 @@
 package com.shop.security;
 
-import com.shop.models.Contact;
-import com.shop.models.Person;
 import com.shop.models.Role;
-import com.shop.services.ContactService;
-import com.shop.services.PersonRoleService;
-import com.shop.services.PersonService;
+import com.shop.services.UserRoleService;
+import com.shop.services.UserService;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -18,33 +15,27 @@ import java.util.Objects;
 
 @Component
 public class UserDetailsServiceImpl implements UserDetailsService {
-    private final PersonService personService;
-    private final ContactService contactService;
-    private final PersonRoleService personRoleService;
+    private final UserService userService;
+    private final UserRoleService userRoleService;
 
     public UserDetailsServiceImpl(
-        PersonService personService,
-        ContactService contactService,
-        PersonRoleService personRoleService
+        UserService userService,
+        UserRoleService userRoleService
     ) {
-        this.personService = personService;
-        this.contactService = contactService;
-        this.personRoleService = personRoleService;
+        this.userService = userService;
+        this.userRoleService = userRoleService;
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) {
-        Person person = personService.findByEmail(email);
-        long id = person.getId();
-        Role role = personRoleService.findRoleForPerson(id);
+        Role role = userRoleService.findRoleForUser(userService.findByEmail(email).getId());
         List<GrantedAuthority> grantedAuthorities = List.of(
             new SimpleGrantedAuthority(role.getName())
         );
-        Contact contact = contactService.findById(id);
 
         return new User(
-            Objects.requireNonNull(contact).getEmail(),
-            contact.getPassword(),
+            Objects.requireNonNull(userService.findByEmail(email)).getEmail(),
+            userService.findByEmail(email).getPassword(),
             grantedAuthorities
         );
     }
