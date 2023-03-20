43dd359339ae221c0487af74b8fc04f1cc8795b5
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-20 21:27:23 +0200

    Update class for implementing interface

--- domain/src/main/java/com/shop/category/CategoryService.java
+++ domain/src/main/java/com/shop/category/CategoryService.java
@@ -1,15 +1,13 @@
 package com.shop.category;
 
-import com.shop.category.Category;
-import com.shop.category.CategoryRepository;
-import com.shop.category.CategoryValidator;
+import com.shop.interfaces.ServiceInterface;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class CategoryService {
+public class CategoryService implements ServiceInterface<Category> {
     private final CategoryRepository categoryRepository;
     private final CategoryValidator categoryValidator;
 
@@ -21,22 +19,19 @@ public class CategoryService {
         this.categoryValidator = categoryValidator;
     }
 
+    @Override
     public List<Category> findAll() {
         return categoryRepository.findAll();
     }
 
+    @Override
     public Category findById(long id) {
         categoryValidator.validate(id, categoryRepository.findAll());
         Optional<Category> category = categoryRepository.findById(id);
         return category.orElseGet(Category::new);
     }
 
-    public Category findByName(String name) {
-        categoryValidator.validate(name, categoryRepository.findAll());
-        Optional<Category> category = categoryRepository.findByName(name);
-        return category.orElseGet(Category::new);
-    }
-
+    @Override
     public Category save(Category category) {
         categoryValidator.validateCategory(category.getName());
         categoryRepository.save(category.getName());
@@ -49,8 +44,15 @@ public class CategoryService {
         return new Category();
     }
 
+    @Override
     public void delete(Category category) {
         categoryValidator.validate(category.getName(), categoryRepository.findAll());
         categoryRepository.delete(category.getName());
+    }
+
+    public Category findByName(String name) {
+        categoryValidator.validate(name, categoryRepository.findAll());
+        Optional<Category> category = categoryRepository.findByName(name);
+        return category.orElseGet(Category::new);
     }
 }
