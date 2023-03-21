d311aeb954600b0b73898561769529decfdcf582
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-22 01:32:26 +0200

    Change admin number for superadmin

--- domain/src/main/resources/db/migration/user/V20220427144125__Insert_data_to_table_user.sql
+++ domain/src/main/resources/db/migration/user/V20220427144125__Insert_data_to_table_user.sql
@@ -1,2 +1,2 @@
 INSERT INTO "user" (first_name, last_name, email, phone, password, admin_number_id)
-VALUES ('admin', 'admin', 'admin', '', '$2a$12$xeqcbrSzWGfjlocTYPbkjeCBjOrWSNVhvpSWdW4Aui5i9Zy6J3OQW', 1);
+VALUES ('admin', 'admin', 'admin', '', '$2a$12$xeqcbrSzWGfjlocTYPbkjeCBjOrWSNVhvpSWdW4Aui5i9Zy6J3OQW', 2);
