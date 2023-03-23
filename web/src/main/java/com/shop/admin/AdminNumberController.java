803f7f88a00039fc181a274efda79f9c2d7dce81
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-03-23 18:01:42 +0200

    Add controller for admin number

---
+++ web/src/main/java/com/shop/admin/AdminNumberController.java
@@ -0,0 +1,48 @@
+package com.shop.admin;
+
+import com.shop.adminnumber.AdminNumber;
+import com.shop.adminnumber.AdminNumberService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(AdminNumberController.ADMIN_NUMBERS_URL)
+public class AdminNumberController {
+    public static final String ADMIN_NUMBERS_URL = "/api/admins-numbers";
+    private final AdminNumberService adminNumberService;
+
+    public AdminNumberController(AdminNumberService adminNumberService) {
+        this.adminNumberService = adminNumberService;
+    }
+
+    @GetMapping
+    public List<AdminNumber> findAll() {
+        return adminNumberService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public AdminNumber findById(@PathVariable long id) {
+        return adminNumberService.findById(id);
+    }
+
+    @PostMapping
+    public AdminNumber save(@RequestBody AdminNumber adminNumber) {
+        return adminNumberService.save(adminNumber);
+    }
+
+    @PostMapping("/{id}")
+    public AdminNumber update(
+        @PathVariable long id,
+        @RequestBody AdminNumber adminNumber
+    ) {
+        adminNumber.setId(id);
+        return adminNumberService.update(adminNumber);
+    }
+
+    @DeleteMapping("/{name}")
+    public String delete(@PathVariable String name) {
+        adminNumberService.delete(AdminNumber.of(name));
+        return "Admin Number Successfully Deleted";
+    }
+}
