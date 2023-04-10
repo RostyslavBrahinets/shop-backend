1e276052926f509ee8489e2d64f6bcc3017d5b77
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2023-04-10 18:56:38 +0300

    Set csrf disabled

--- web/src/main/java/com/shop/configs/SecurityConfig.java
+++ web/src/main/java/com/shop/configs/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -32,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        http.csrf().disable();
 
         http
             .authorizeRequests()
