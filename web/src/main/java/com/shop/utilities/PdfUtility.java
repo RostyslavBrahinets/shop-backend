64fb950afde63ee4eaa06731f689056515cf8c4f
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-03 20:52:26 +0300

    Update utility for pdf

--- web/src/main/java/com/shop/utilities/PdfUtility.java
+++ web/src/main/java/com/shop/utilities/PdfUtility.java
@@ -12,72 +12,75 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.awt.*;
 import java.io.IOException;
 
-public class PdfUtility {
-    private final ReportDto reportInfo;
+import static com.lowagie.text.Element.ALIGN_CENTER;
 
-    public PdfUtility(ReportDto reportInfo) {
-        this.reportInfo = reportInfo;
+public class PdfUtility {
+    private final ReportDto report;
+
+    public PdfUtility(ReportDto report) {
+        this.report = report;
     }
 
     public void export(HttpServletResponse response) throws DocumentException, IOException {
-        Document document = new Document(PageSize.A4);
-        PdfWriter.getInstance(document, response.getOutputStream());
+        try (Document document = new Document(PageSize.A4)) {
+            PdfWriter.getInstance(document, response.getOutputStream());
 
-        document.open();
-        Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-        font.setSize(18);
-        font.setColor(Color.BLACK);
+            document.open();
 
-        Paragraph p = new Paragraph("Report", font);
-        p.setAlignment(Paragraph.ALIGN_CENTER);
+            Paragraph p = new Paragraph("Report", getFont(32, true));
+            p.setAlignment(ALIGN_CENTER);
 
-        document.add(p);
+            document.add(p);
 
-        PdfPTable table = new PdfPTable(4);
-        table.setWidthPercentage(100f);
-        table.setWidths(new float[]{1.5f, 3.5f, 3.0f, 3.0f});
-        table.setSpacingBefore(10);
+            PdfPTable table = new PdfPTable(2);
+            table.setWidthPercentage(100f);
+            table.setWidths(new float[]{3.0f, 1.5f});
+            table.setSpacingBefore(10);
 
-        writeTableHeader(table);
-        writeTableData(table);
+            writeTableHeader(table);
+            writeTableData(table);
 
-        document.add(table);
-
-        document.close();
+            document.add(table);
+        }
     }
 
     private void writeTableHeader(PdfPTable table) {
         PdfPCell cell = new PdfPCell();
-        cell.setBackgroundColor(Color.WHITE);
-        cell.setPadding(4);
+        cell.setBorderColor(Color.WHITE);
+        cell.setPadding(2);
 
-        Font font = FontFactory.getFont(FontFactory.HELVETICA);
-        font.setColor(Color.BLACK);
-
-        cell.setPhrase(new Phrase("Name", font));
+        cell.setPhrase(new Phrase("Name", getFont(18, true)));
         table.addCell(cell);
 
-        cell.setPhrase(new Phrase("Price", font));
-        table.addCell(cell);
-
-        cell.setPhrase(new Phrase("Total Cost", font));
-        table.addCell(cell);
-
-        cell.setPhrase(new Phrase("Amount Of Money", font));
+        cell.setPhrase(new Phrase("Price", getFont(18, true)));
         table.addCell(cell);
     }
 
     private void writeTableData(PdfPTable table) {
-        for (Product product : reportInfo.getProducts()) {
-            table.addCell(product.getName());
-            table.addCell(String.valueOf(product.getPrice()));
-            table.addCell("");
-            table.addCell("");
+        PdfPCell cell = new PdfPCell();
+        cell.setBorderColor(Color.WHITE);
+        cell.setPadding(2);
+
+        for (Product product : report.products()) {
+            cell.setPhrase(new Phrase(product.getName(), getFont(18, false)));
+            table.addCell(cell);
+
+            cell.setPhrase(new Phrase(String.valueOf(product.getPrice()), getFont(18, false)));
+            table.addCell(cell);
         }
 
-        table.addCell("");
-        table.addCell("");
-        table.addCell(String.valueOf(reportInfo.getTotalCost()));
-        table.addCell(String.valueOf(reportInfo.getAmountOfMoney()));
+        cell.setPhrase(new Phrase("Amount Price", getFont(18, true)));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase(String.valueOf(report.priceAmount()), getFont(18, true)));
+        table.addCell(cell);
+    }
+
+    private static Font getFont(int size, boolean isBold) {
+        String style = isBold ? FontFactory.HELVETICA_BOLD : FontFactory.HELVETICA;
+        Font font = FontFactory.getFont(style);
+        font.setSize(size);
+        font.setColor(Color.BLACK);
+        return font;
     }
 }
