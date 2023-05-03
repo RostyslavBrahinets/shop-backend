482e7e66d93f04fbbb8389bb09918161eea201bb
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-03 20:38:59 +0300

    Update dto for report

--- web/src/main/java/com/shop/report/ReportDto.java
+++ web/src/main/java/com/shop/report/ReportDto.java
@@ -1,42 +1,11 @@
 package com.shop.report;
 
 import com.shop.product.Product;
-import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Component
-public class ReportDto {
-    private List<Product> products;
-    private double totalCost;
-    private double amountOfMoney;
-
-    public ReportDto() {
-        this.products = new ArrayList<>();
-    }
-
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
-
-    public double getTotalCost() {
-        return totalCost;
-    }
-
-    public void setTotalCost(double totalCost) {
-        this.totalCost = totalCost;
-    }
-
-    public double getAmountOfMoney() {
-        return amountOfMoney;
-    }
-
-    public void setAmountOfMoney(double amountOfMoney) {
-        this.amountOfMoney = amountOfMoney;
-    }
+public record ReportDto(
+    List<Product> products,
+    double priceAmount
+) {
 }
