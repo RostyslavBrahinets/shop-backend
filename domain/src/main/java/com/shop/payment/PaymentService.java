c5593c9b3f9fd1533b7c77236458cfdbb00f6bc8
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-03 12:13:44 +0300

    Add method for creating payment to service

--- domain/src/main/java/com/shop/payment/PaymentService.java
+++ domain/src/main/java/com/shop/payment/PaymentService.java
@@ -1,7 +1,43 @@
 package com.shop.payment;
 
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentMethod;
+import com.stripe.param.PaymentMethodCreateParams;
+import com.stripe.param.PaymentMethodCreateParams.CardDetails;
+import com.stripe.param.PaymentMethodCreateParams.Type;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @Service
+@PropertySource("classpath:application.properties")
 public class PaymentService {
+    private final String stripeSecretKey;
+
+    public PaymentService(
+        @Value("${stripe.secret.key}") String stripeSecretKey
+    ) {
+        this.stripeSecretKey = stripeSecretKey;
+    }
+
+    private PaymentMethod createPaymentMethod(
+        String cardNumber,
+        String cardExpiry,
+        String cardCvc
+    ) throws StripeException {
+        Stripe.apiKey = stripeSecretKey;
+
+        PaymentMethodCreateParams params = PaymentMethodCreateParams.builder()
+            .setType(Type.CARD)
+            .setCard(CardDetails.builder()
+                .setNumber(cardNumber)
+                .setExpMonth(Long.parseLong(cardExpiry.split("/")[0]))
+                .setExpYear(Long.parseLong("20" + cardExpiry.split("/")[1]))
+                .setCvc(cardCvc)
+                .build())
+            .build();
+
+        return PaymentMethod.create(params);
+    }
 }
