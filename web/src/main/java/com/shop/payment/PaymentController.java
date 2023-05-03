af3363ebad22f78cce4d6afa310d51c83854fe2b
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-03 12:20:03 +0300

    Update method for payment

--- web/src/main/java/com/shop/payment/PaymentController.java
+++ web/src/main/java/com/shop/payment/PaymentController.java
@@ -1,18 +1,10 @@
 package com.shop.payment;
 
-import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
-import com.stripe.model.Charge;
-import com.stripe.param.ChargeCreateParams;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
 
 @RestController
 @RequestMapping(PaymentController.PAYMENT_URL)
@@ -26,26 +18,18 @@ public class PaymentController {
     }
 
     @PostMapping
-    public ResponseEntity<String> createCharge(
-        @RequestParam String token,
-        @RequestParam BigDecimal amount,
-        @RequestParam String currency
-    ) {
-        Stripe.apiKey = stripeSecretKey;
-
-        ChargeCreateParams params =
-            ChargeCreateParams.builder()
-                .setAmount(amount.multiply(new BigDecimal(100)).longValueExact())
-                .setCurrency(currency)
-                .setDescription("Charge")
-                .setSource(token)
-                .build();
-
+    public PaymentResponseDto processPayment(@RequestBody PaymentRequestDto paymentRequestDto) {
         try {
-            Charge charge = Charge.create(params);
-            return ResponseEntity.ok("Payment succeeded! Id: " + charge.getId());
+            String paymentInfo = paymentService.payment(
+                paymentRequestDto.amount(),
+                paymentRequestDto.cardNumber(),
+                paymentRequestDto.cardExpiry(),
+                paymentRequestDto.cardCvc()
+            );
+
+            return new PaymentResponseDto(paymentInfo);
         } catch (StripeException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
+            return new PaymentResponseDto(e.getMessage());
         }
     }
 }
