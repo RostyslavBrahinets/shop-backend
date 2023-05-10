9b5af81669ae51453a3255d8cc111d38dc529ef6
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-10 13:38:54 +0300

    Change password of superadmin for more secure

--- domain/src/main/resources/db/migration/user/V20220427144125__Insert_data_to_table_user.sql
+++ domain/src/main/resources/db/migration/user/V20220427144125__Insert_data_to_table_user.sql
@@ -1,2 +1,2 @@
 INSERT INTO "user" (first_name, last_name, email, phone, password, admin_number)
-VALUES ('admin', 'admin', 'admin', '', '$2a$12$xeqcbrSzWGfjlocTYPbkjeCBjOrWSNVhvpSWdW4Aui5i9Zy6J3OQW', '13012002');
+VALUES ('admin', 'admin', 'admin', '', '$2a$12$NBYDZTTPC0zmGCXBL1LQKejobjbbMgJ4oKh1vZIIJIikcfw2A57X6', '13012002');
