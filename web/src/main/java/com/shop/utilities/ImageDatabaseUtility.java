de975fd9adadbef5d121bbb7af3d4c224165bfc6
rbrahinets <rostyslav.barhinets@gmail.com>
2024-04-14 19:00:00 +0300

    refactor ImageDatabaseUtility

--- web/src/main/java/com/shop/utilities/ImageDatabaseUtility.java
+++ web/src/main/java/com/shop/utilities/ImageDatabaseUtility.java
@@ -1,8 +1,8 @@
 package com.shop.utilities;
 
-import com.shop.product.ProductRepository;
 import com.shop.db.DatabaseTemplate;
 import com.shop.product.Product;
+import com.shop.product.ProductRepository;
 
 import java.io.File;
 import java.util.List;
@@ -12,12 +12,11 @@ public class ImageDatabaseUtility {
     public void setImagesToDatabase(List<String> images) throws Exception {
         ProductRepository productRepository = new ProductRepository(DatabaseTemplate.getJdbcTemplate());
         for (int i = 0; i < images.size(); i++) {
-            Optional<Product> product = productRepository.findById(i + 1);
-            if (product.isPresent()) {
-                if (product.get().getImage() == null) {
-                    byte[] byteImage = ImageUtility.imageToBytes(new File(images.get(i)));
-                    productRepository.saveImageForProduct(byteImage, i + 1);
-                }
+            Optional<Product> product = productRepository.findById(i + 1L);
+            if (product.isPresent() && (product.get().getImage() == null)) {
+                byte[] byteImage = ImageUtility.imageToBytes(new File(images.get(i)));
+                productRepository.saveImageForProduct(byteImage, i + 1L);
+
             }
         }
     }
