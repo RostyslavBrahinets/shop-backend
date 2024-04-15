7eb5246c1090433d793f637a60594eddfa36f05e
rbrahinets <rostyslav.barhinets@gmail.com>
2024-04-15 21:13:12 +0300

    Add class with parameters for testing admin numbers

---
+++ domain/src/test/java/com/shop/adminnumber/AdminNumberParameter.java
@@ -0,0 +1,45 @@
+package com.shop.adminnumber;
+
+import java.util.List;
+
+public class AdminNumberParameter {
+    static AdminNumber getAdminNumberWithId() {
+        return getAdminNumberWithoutId().withId(getAdminNumberId());
+    }
+
+    static AdminNumber getAdminNumberWithId2() {
+        return getAdminNumberWithoutId2().withId(getAdminNumberId2());
+    }
+
+    static AdminNumber getAdminNumberWithId2(long id) {
+        return getAdminNumberWithoutId2().withId(id);
+    }
+
+    static AdminNumber getAdminNumberWithoutId() {
+        return AdminNumber.of(getNumber());
+    }
+
+    static AdminNumber getAdminNumberWithoutId2() {
+        return AdminNumber.of(getNumber2());
+    }
+
+    static long getAdminNumberId() {
+        return 1L;
+    }
+
+    static long getAdminNumberId2() {
+        return 2L;
+    }
+
+    static String getNumber() {
+        return "12345678";
+    }
+
+    static String getNumber2() {
+        return "87654321";
+    }
+
+    static List<AdminNumber> getAdminNumbers() {
+        return List.of();
+    }
+}
