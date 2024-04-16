bf251cc6123146411fdd6cb559db47eaa45d921d
rbrahinets <rostyslav.barhinets@gmail.com>
2024-04-16 13:24:29 +0300

    Add class with parameters for testing category

---
+++ domain/src/test/java/com/shop/category/CategoryParameter.java
@@ -0,0 +1,41 @@
+package com.shop.category;
+
+import java.util.List;
+
+public class CategoryParameter {
+    static Category getCategoryWithId() {
+        return getCategoryWithoutId().withId(getCategoryId());
+    }
+
+    static Category getCategoryWithId(long id, String name) {
+        return getCategoryWithoutId(name).withId(id);
+    }
+
+    static Category getCategoryWithoutId() {
+        return Category.of(getName());
+    }
+
+    static Category getCategoryWithoutId(String name) {
+        return Category.of(name);
+    }
+
+    static long getCategoryId() {
+        return 1L;
+    }
+
+    static long getCategoryId2() {
+        return 2L;
+    }
+
+    static String getName() {
+        return "name";
+    }
+
+    static String getName2() {
+        return "name2";
+    }
+
+    static List<Category> getCategories() {
+        return List.of();
+    }
+}
