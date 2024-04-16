f6c3e5c19bd0181e82f0e8e1904434889aed2d13
rbrahinets <rostyslav.barhinets@gmail.com>
2024-04-16 13:23:39 +0300

    Refactor tests for service of cart

--- domain/src/test/java/com/shop/cart/CartServiceTest.java
+++ domain/src/test/java/com/shop/cart/CartServiceTest.java
@@ -1,6 +1,5 @@
 package com.shop.cart;
 
-import com.shop.user.User;
 import com.shop.user.UserService;
 import com.shop.user.UserValidator;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +11,8 @@ import org.mockito.MockitoAnnotations;
 import java.util.List;
 import java.util.Optional;
 
+import static com.shop.cart.CartParameter.*;
+import static com.shop.user.UserParameter.getUserWithId;
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
@@ -42,16 +43,6 @@ class CartServiceTest {
     }
 
     @Test
-    @DisplayName("Cart was saved for with correct input")
-    void cart_was_saved_with_correct_input() {
-        Cart savedCart = cartService.save(Cart.of(0, 1).withId(1));
-
-        verify(cartRepository).save(Cart.of(0, 1).withId(1));
-
-        assertThat(savedCart).isEqualTo(new Cart(1, 0, 1));
-    }
-
-    @Test
     @DisplayName("Empty list of carts is returned in case when no carts in storage")
     void empty_list_of_carts_is_returned_in_case_when_no_carts_in_storage() {
         when(cartRepository.findAll()).thenReturn(emptyList());
@@ -65,44 +56,52 @@ class CartServiceTest {
     @DisplayName("List of carts is returned in case when carts are exists in storage")
     void list_of_carts_is_returned_in_case_when_carts_are_exists_in_storage() {
         when(cartRepository.findAll()).thenReturn(
-            List.of(
-                Cart.of(0, 0).withId(1)
-            )
+            List.of(getCartWithId())
         );
 
         List<Cart> carts = cartService.findAll();
 
-        assertThat(carts).isEqualTo(List.of(new Cart(1, 0, 0)));
+        assertThat(carts).isEqualTo(List.of(getCartWithId()));
     }
 
     @Test
     @DisplayName("Cart was found by id")
     void cart_was_found_by_id() {
         when(cartRepository.findById(1)).thenReturn(
-            Optional.of(Cart.of(0, 0).withId(1))
+            Optional.of(getCartWithId())
         );
 
-        Cart cart = cartService.findById(1);
+        Cart cart = cartService.findById(getCartId());
 
-        assertThat(cart).isEqualTo(new Cart(1, 0, 0));
+        assertThat(cart).isEqualTo(getCartWithId());
     }
 
     @Test
-    @DisplayName("Cart was found by user")
-    void cart_was_found_by_user() {
-        when(cartRepository.findByUser(User.of(null, null).withId(1))).thenReturn(
-            Optional.of(Cart.of(0, 0).withId(1))
-        );
+    @DisplayName("Cart was saved for with correct input")
+    void cart_was_saved_with_correct_input() {
+        Cart savedCart = cartService.save(getCartWithoutId());
 
-        Cart cart = cartService.findByUser(User.of(null, null).withId(1));
+        verify(cartRepository).save(getCartWithId());
 
-        assertThat(cart).isEqualTo(new Cart(1, 0, 0));
+        assertThat(savedCart).isEqualTo(getCartWithId());
     }
 
     @Test
     @DisplayName("Cart was deleted")
     void cart_was_deleted() {
-        cartService.delete(Cart.of(0, 0).withId(1));
-        verify(cartRepository).delete(Cart.of(0, 0).withId(1));
+        cartService.delete(getCartWithId());
+        verify(cartRepository).delete(getCartWithId());
+    }
+
+    @Test
+    @DisplayName("Cart was found by user")
+    void cart_was_found_by_user() {
+        when(cartRepository.findByUser(getUserWithId())).thenReturn(
+            Optional.of(getCartWithId())
+        );
+
+        Cart cart = cartService.findByUser(getUserWithId());
+
+        assertThat(cart).isEqualTo(getCartWithId());
     }
 }
