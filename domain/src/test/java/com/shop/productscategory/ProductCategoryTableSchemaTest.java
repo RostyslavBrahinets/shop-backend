51558cf4833924b87f3e9734ac63a54212d92aca
rbrahinets <rostyslav.barhinets@gmail.com>
2024-04-18 13:12:40 +0300

    Refactor tests for table schema of product category

--- domain/src/test/java/com/shop/productscategory/ProductCategoryTableSchemaTest.java
+++ domain/src/test/java/com/shop/productscategory/ProductCategoryTableSchemaTest.java
@@ -13,7 +13,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.jdbc.JdbcTestUtils;
 
-import static com.shop.SqlMigrationClasspath.*;
+import static com.shop.SqlMigrationClasspath.PRODUCT_CATEGORY;
+import static com.shop.product.ProductParameter.getCategoryId;
+import static com.shop.product.ProductParameter.getProductId;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 @JdbcTest
@@ -40,7 +42,7 @@ class ProductCategoryTableSchemaTest {
     void failed_to_insert_null_product_id_value() {
         var params = new MapSqlParameterSource();
         params.addValue("product_id", null);
-        params.addValue("category_id", 1);
+        params.addValue("category_id", getCategoryId());
 
         assertThatCode(
             () -> jdbcTemplate.update(
@@ -56,7 +58,7 @@ class ProductCategoryTableSchemaTest {
     @DisplayName("Failed to insert null category id value")
     void failed_to_insert_null_category_id_value() {
         var params = new MapSqlParameterSource();
-        params.addValue("product_id", 1);
+        params.addValue("product_id", getProductId());
         params.addValue("category_id", null);
 
         assertThatCode(
