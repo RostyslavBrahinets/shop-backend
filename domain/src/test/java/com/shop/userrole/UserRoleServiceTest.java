04b3c94579332814d049ab9b3027e39744b76f4f
rbrahinets <rostyslav.barhinets@gmail.com>
2024-04-18 13:17:03 +0300

    Refactor tests for service of user role

--- domain/src/test/java/com/shop/userrole/UserRoleServiceTest.java
+++ domain/src/test/java/com/shop/userrole/UserRoleServiceTest.java
@@ -1,14 +1,16 @@
 package com.shop.userrole;
 
+import com.shop.role.RoleValidator;
 import com.shop.user.UserService;
 import com.shop.user.UserValidator;
-import com.shop.role.RoleValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static com.shop.role.RoleParameter.getRoleId;
+import static com.shop.user.UserParameter.getUserId;
 import static org.mockito.Mockito.verify;
 
 class UserRoleServiceTest {
@@ -38,7 +40,7 @@ class UserRoleServiceTest {
     @Test
     @DisplayName("Role for user was saved")
     void role_for_user_was_saved() {
-        userRoleService.saveRoleForUser(1, 1);
-        verify(userRoleRepository).saveRoleForUser(1, 1);
+        userRoleService.saveRoleForUser(getUserId(), getRoleId());
+        verify(userRoleRepository).saveRoleForUser(getUserId(), getRoleId());
     }
 }
