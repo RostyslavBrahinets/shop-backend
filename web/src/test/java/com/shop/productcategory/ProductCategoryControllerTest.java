a044bc8bf98fb99c600fa92e92998f7cbe39039c
rbrahinets <rostyslav.barhinets@gmail.com>
2024-04-22 20:54:37 +0300

    Update tests for product category controller

--- web/src/test/java/com/shop/productcategory/ProductCategoryControllerTest.java
+++ web/src/test/java/com/shop/productcategory/ProductCategoryControllerTest.java
@@ -7,19 +7,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.MockBeans;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static com.shop.category.CategoryParameter.getCategoryId;
+import static com.shop.category.CategoryParameter.getCategoryWithId;
+import static com.shop.product.ProductParameter.getBarcode;
 import static com.shop.product.ProductParameter.getProductWithId;
 import static com.shop.productcategory.ProductCategoryController.PRODUCT_CATEGORY_URL;
+import static com.shop.productcategory.ProductCategoryParameter.getProductCategory;
+import static com.shop.utilities.JsonUtility.getJsonBody;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @MockBeans({
@@ -51,5 +56,48 @@ class ProductCategoryControllerTest {
             .andExpect(status().isOk())
             .andExpect(content().contentType(APPLICATION_JSON))
             .andExpect(jsonPath("$").isArray());
+    }
+
+    @Test
+    @DisplayName("Category for product found")
+    void category_for_product_found() throws Exception {
+        when(productCategoryService.findCategoryForProduct(getBarcode()))
+            .thenReturn(getCategoryWithId());
+
+        mockMvc.perform(get(PRODUCT_CATEGORY_URL + String.format("/%s", getBarcode()))
+                .with(user("admin").password("admin").roles("ADMIN"))
+                .with(csrf()))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Product to category saved")
+    void product_to_category_saved() throws Exception {
+        mockMvc.perform(post(PRODUCT_CATEGORY_URL)
+                .with(user("admin").password("admin").roles("ADMIN"))
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(getJsonBody(getProductCategory())))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Product to category updated")
+    void product_to_category_updated() throws Exception {
+        mockMvc.perform(put(PRODUCT_CATEGORY_URL)
+                .with(user("admin").password("admin").roles("ADMIN"))
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(getJsonBody(getProductCategory())))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Product from category deleted")
+    void product_from_category_deleted() throws Exception {
+        mockMvc.perform(delete(PRODUCT_CATEGORY_URL + String.format("/%s", getBarcode()))
+                .with(user("admin").password("admin").roles("ADMIN"))
+                .with(csrf()))
+            .andExpect(status().isOk());
     }
 }
