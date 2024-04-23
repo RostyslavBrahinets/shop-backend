4852f81db566fa8925ac10e6596903773194eadb
rbrahinets <rostyslav.barhinets@gmail.com>
2024-04-23 10:13:50 +0300

    Update parameters for testing admin number

--- web/src/test/java/com/shop/adminnumber/AdminNumberParameter.java
+++ web/src/test/java/com/shop/adminnumber/AdminNumberParameter.java
@@ -1,6 +1,26 @@
 package com.shop.adminnumber;
 
 public class AdminNumberParameter {
+    static AdminNumber getAdminNumberWithId() {
+        return getAdminNumberWithoutId().withId(getAdminNumberId());
+    }
+
+    static AdminNumber getAdminNumberWithId2() {
+        return getAdminNumberWithoutId2().withId(getAdminNumberId());
+    }
+
+    static AdminNumber getAdminNumberWithoutId() {
+        return AdminNumber.of(getNumber());
+    }
+
+    static AdminNumber getAdminNumberWithoutId2() {
+        return AdminNumber.of("87654321");
+    }
+
+    static long getAdminNumberId() {
+        return 1L;
+    }
+
     public static String getNumber() {
         return "12345678";
     }
